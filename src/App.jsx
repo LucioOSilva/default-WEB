270fe7bda7412fbec58633d8baea867d63a84c95
lucioosilva <lucioosilva00@gmail.com>
2022-07-05 16:58:58 -0300

    Updates App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import React from 'react';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import Home from './presentation/pages/home/Home';
+import NotFound from './presentation/pages/notFound/NotFound';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
